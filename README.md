7cb408e09b8200655ce156936f255d7c82d0bd5a
adamhamdi85 <adamhamdi85@gmail.com>
2022-10-13 14:51:29 +0100

    README.md for OOP

--- README.md
+++ README.md
@@ -40,3 +40,66 @@ number = 23.66
 print(math.ceil(number))  # ceil will help round up
 print(math.floor(number))  # floor will help round down
 ```
+```python
+
+DRY - Don't repeat yourself
+reusable - saves time - saves money
+building functions
+syntax def name():
+def greeting():
+    # greet user
+    print("Hello Dear ")
+
+    pass
+    keyword called pass
+the function needs to be called
+greeting()
+
+greet user with their name
+def greet_user(name):    # create a function that takes and argument(name)
+    print("Hello Dear " + name)    # adding 2 string with user input()
+    #pass
+greet_user("Adam")
+
+create a function that takes int as an argument
+def add(value1, value2):    # take user input as int then add them together display the outcome
+    #print(value1 + value2)
+# return_statement
+    return value1 + value2
+# if you are using a return statement and caling the function - it needs to be in a print statement
+print(add(2, 2))
+
+
+
+add(2, 2)
+
+def multiply (value1 , value2):
+    print(value1 * value2)
+    pass
+
+multiply(2,2)
+
+def divide (value1, value2):
+    print(value1 / value2)
+    pass
+
+divide(2,2)
+
+def modulus (value1, value2):
+    print( value1 / value2)
+    pass
+modulus(2,2)
+#
+def divide(value1, value2):
+    return value1 / value2
+print(divide(2,2))
+ 
+ def multiply(value1, value2):
+     return value1 * value2
+ print(multiply(2,2))
+ def modulo(value1, value2):
+     return value1 % value2
+ print(modulo(2,2))
+
+
+```
--- oop_modules_packages.py
+++ oop_modules_packages.py
@@ -22,12 +22,78 @@ import math
 
 from random import *
 import math
+# import datetime, sys
+# import os
+# print(os.cpu_count())   # this will result in number of cpu based on your OS
+# print(datetime.date.today())  # this will result today's date based on your OS
+# print(sys.path)
 
 
-number = 23.66
+# number = 23.66
 
 # any numbers - to round the figure up -
 # number 1.50 above to round up to 2
 # number 1.49 or less round down to 1
-print(math.ceil(number))  # ceil will help round up
-print(math.floor(number))  # floor will help round down
+# print(math.ceil(number))  # ceil will help round up
+# print(math.floor(number))  # floor will help round down
+
+
+# DRY - Don't repeat yourself
+# reusable - saves time - saves money
+# building functions
+# syntax def name():
+# def greeting():
+#     # greet user
+#     print("Hello Dear ")
+
+    #pass
+    # keyword called pass
+# the function needs to be called
+# greeting()
+
+# greet user with their name
+# def greet_user(name):    # create a function that takes and argument(name)
+#     print("Hello Dear " + name)    # adding 2 string with user input()
+#     #pass
+# greet_user("Adam")
+
+#create a function that takes int as an argument
+# def add(value1, value2):    # take user input as int then add them together display the outcome
+#     #print(value1 + value2)
+# # return_statement
+#     return value1 + value2
+# # if you are using a return statement and caling the function - it needs to be in a print statement
+# print(add(2, 2))
+#
+#
+#
+# add(2, 2)
+
+# def multiply (value1 , value2):
+#     print(value1 * value2)
+#     pass
+#
+# multiply(2,2)
+#
+# def divide (value1, value2):
+#     print(value1 / value2)
+#     pass
+#
+# divide(2,2)
+#
+# def modulus (value1, value2):
+#     print( value1 / value2)
+#     pass
+# modulus(2,2)
+# #
+# def divide(value1, value2):
+#     return value1 / value2
+# print(divide(2,2))
+#
+# def multiply(value1, value2):
+#     return value1 * value2
+# print(multiply(2,2))
+# def modulo(value1, value2):
+#     return value1 % value2
+# print(modulo(2,2))
+
